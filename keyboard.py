0b2e44bcf9c04784ee28e06c488a4f0b7fa3041f
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2019-07-17 08:42:16 +0100

    updating keyboard

--- keyboard.py
+++ keyboard.py
@@ -5,7 +5,8 @@
 
 import colorsys
 import random
-from evdev import InputDevice, ecodes
+import asyncio
+from evdev import InputDevice, ecodes, util, events
 
 from openrazer.client import DeviceManager
 # from openrazer.client import constants as razer_constants
@@ -23,30 +24,43 @@ device_manager.sync_effects = False
 
 
 # Helper funciton to generate interesting colors
-def random_color():
-    rgb = colorsys.hsv_to_rgb(
-        random.uniform(0.7, 0.75), random.uniform(0.8, 1), 1)
-    return tuple(map(lambda x: int(256 * x), rgb))
+class Effect:
+    pass
 
 
 class KeyboardConfig(object):
 
     """Set config fo """
 
-    def __init__(self, device, keyboard_input):
+    def __init__(self, device, keyboard_input, keyboard_input_1):
+        self.saturation = 100
+        self.actual_color = 120 # random.uniform(0, 255)
+        self.increment = 0
         self.device = device
         self.keyboard_input = keyboard_input
+        self.keyboard_input_1 = keyboard_input_1
         self.rows = device.fx.advanced.rows
         self.cols = device.fx.advanced.cols
-        self.numpad_enable = 0 in keyboard_input.leds()
-        self.caps_locks_enable = 1 in keyboard_input.leds()
+        self.numpad_enable = 0 in keyboard_input_1.leds()
+        self.caps_locks_enable = 1 in keyboard_input_1.leds()
+        self.generate_color()
         self.update_lights()
 
+    def generate_color(self):
+        # self.actual_color = (self.actual_color + self.increment) % 255
+        next = self.actual_color + 100
+        actual_color = random.uniform(self.actual_color, next) % 255
+        rgb = colorsys.hsv_to_rgb(actual_color / 255, self.saturation / 100, 1)
+        self.color_rgb = list(map(lambda x: int(255 * x), rgb))
+
+    def update_saturation(self):
+        rgb = colorsys.hsv_to_rgb(self.actual_color / 255, self.saturation / 100, 1)
+        self.color_rgb = list(map(lambda x: int(255 * x), rgb))
+
     def update_lights(self):
-        self.normal_state()
-        if self.numpad_enable:
-            self.numpad_color(*random_color())
-        else:
+        color = self.color_rgb
+        self.normal_state(color)
+        if not self.numpad_enable:
             self.numpad_color(0, 0, 0)
 
         if self.caps_locks_enable:
@@ -54,10 +68,12 @@ class KeyboardConfig(object):
 
         self.device.fx.advanced.draw()
 
-    def normal_state(self):
+    def normal_state(self, color):
         for row in range(self.rows):
-            for col in range(self.cols - 4):
-                self.device.fx.advanced.matrix[row, col] = random_color()
+            for col in range(self.cols):
+                self.generate_color()
+                color = self.color_rgb
+                self.device.fx.advanced.matrix[row, col] = color
 
     def numpad_color(self, *color):
         for row in range(self.rows):
@@ -69,18 +85,45 @@ class KeyboardConfig(object):
             for col in range(2, self.cols - 8):
                 self.device.fx.advanced.matrix[row, col] = color
 
-    def listening(self):
-        for event in self.keyboard_input.read_loop():
+    async def process(self, device):
+        async for event in device.async_read_loop():
             if event.type == ecodes.EV_LED and event.code == 1:
                 self.caps_locks_enable = bool(event.value)
+                self.update_lights()
             elif event.type == ecodes.EV_LED and event.code == 0:
                 self.numpad_enable = bool(event.value)
+                self.update_lights()
+            else:
+                key = util.categorize(event)
+                if isinstance(key, events.KeyEvent) and key.keystate == 1:
+                    pass
+                    # self.saturation -= 5
+                    # if self.saturation < 0:
+                    #     self.saturation = 0
+                    # self.generate_color()
+                    # self.update_lights()
 
+    async def increase_saturation(self):
+        while True:
+            self.saturation += 5
+            if self.saturation > 100:
+                self.saturation = 100
+            self.update_saturation()
             self.update_lights()
+            await asyncio.sleep(0.7)
+
+    def listening(self):
+        for device in self.keyboard_input, self.keyboard_input_1:
+            asyncio.ensure_future(self.process(device))
+
+        loop = asyncio.get_event_loop()
+        # asyncio.ensure_future(self.increase_saturation())
+        loop.run_forever()
 
 
 dev = InputDevice('/dev/input/by-id/usb-Razer_Razer_Ornata_Chroma-event-kbd')
+dev1 = InputDevice('/dev/input/by-id/usb-Razer_Razer_Ornata_Chroma-if01-event-kbd')
 
-config = KeyboardConfig(device_manager.devices[0], dev)
+config = KeyboardConfig(device_manager.devices[0], dev1, dev)
 
 config.listening()
